766055347ff0f36a80bf6aad552c115f7479e84a
Agua <aguassantasnunez@gmail.com>
2025-03-25 16:13:44 +0100

    archivo js

---
+++ app.js
@@ -0,0 +1,17 @@
+function Seguro (marca,year,tipo){
+    this.marca = marca;
+    this.year= year;
+    this.tipo = tipo;
+}
+
+function UI(){
+
+    UI.prototype.llenarOpciones = ()=>{
+        const max = new Date().getFullYear(),
+        min = max - 20;
+    }
+
+}
+document.addEventListener('DOMContentLoaded', ()=> {
+
+})
