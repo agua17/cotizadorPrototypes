1ad4b9bddcd3eb321907522971e74ce4fa97b5e9
Agua <aguassantasnunez@gmail.com>
2025-03-31 16:02:27 +0200

    codigo_eleccion_año

--- app.js
+++ app.js
@@ -4,14 +4,40 @@ function Seguro (marca,year,tipo){
     this.tipo = tipo;
 }
 
-function UI(){
+function UI() {}
 
     UI.prototype.llenarOpciones = ()=>{
         const max = new Date().getFullYear(),
         min = max - 20;
+
+        const selectYear = document.querySelector('#year');
+
+        for (let i = max; i > min; i--) {
+            let option = document.createElement('option');
+                option.value = i; 
+                option.textContent = i; 
+                selectYear.appendChild(option);
+            
+        } 
     }
 
-}
+
+
+const ui = new UI();
+console.log(ui);
+
 document.addEventListener('DOMContentLoaded', ()=> {
+    ui.llenarOpciones();
 
 })
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizador-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+    console.log ('Cotizando...');
+}
